e51ae2f454affe584087da1f67183748057d5ab2
Chitu Dorin <46376664+chitudorin@users.noreply.github.com>
2022-12-10 15:36:30 +0200

    Add play round function

--- script.js
+++ script.js
@@ -4,3 +4,46 @@ function getComputerChoice() {
   let choice = options[Math.floor(Math.random() * options.length)];
   return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    function draw() {
+      return `It's a draw!`;
+    }
+  
+    function lose() {
+      computerSelection =
+        computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+      return `You lose! ${computerSelection} beats ${playerSelection}!`;
+    }
+  
+    function win() {
+      playerSelection =
+        playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+      return `You win! ${playerSelection} beats ${computerSelection}!`;
+    }
+  
+    if (playerSelection === computerSelection) {
+      return draw();
+    } else if (playerSelection === "rock") {
+      if (computerSelection === "paper") {
+        return lose();
+      } else {
+        return win();
+      }
+    } else if (playerSelection === "paper") {
+      if (computerSelection === "scissors") {
+        return lose();
+      } else {
+        return win();
+      }
+    } else {
+      if (computerSelection === "rock") {
+        return lose();
+      } else {
+        return win();
+      }
+    }
+  }
+  
+  console.log(playRound(getComputerChoice(), getComputerChoice()));
