feb75985ef82eb664a6a3bb66fc91c9e1e9b8bbf
Chitu Dorin <46376664+chitudorin@users.noreply.github.com>
2022-12-10 15:52:08 +0200

    Add 5 round game function

--- script.js
+++ script.js
@@ -6,44 +6,67 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    function draw() {
-      return `It's a draw!`;
-    }
-  
-    function lose() {
-      computerSelection =
-        computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
-      return `You lose! ${computerSelection} beats ${playerSelection}!`;
-    }
-  
-    function win() {
-      playerSelection =
-        playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-      return `You win! ${playerSelection} beats ${computerSelection}!`;
-    }
-  
-    if (playerSelection === computerSelection) {
-      return draw();
-    } else if (playerSelection === "rock") {
-      if (computerSelection === "paper") {
-        return lose();
-      } else {
-        return win();
-      }
-    } else if (playerSelection === "paper") {
-      if (computerSelection === "scissors") {
-        return lose();
-      } else {
-        return win();
-      }
+  playerSelection = playerSelection.toLowerCase();
+  function draw() {
+    return `It's a draw!`;
+  }
+
+  function lose() {
+    computerSelection =
+      computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+    return `You lose! ${computerSelection} beats ${playerSelection}!`;
+  }
+
+  function win() {
+    playerSelection =
+      playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    return `You win! ${playerSelection} beats ${computerSelection}!`;
+  }
+
+  if (playerSelection === computerSelection) {
+    return draw();
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
+      return lose();
     } else {
-      if (computerSelection === "rock") {
-        return lose();
-      } else {
-        return win();
-      }
+      return win();
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "scissors") {
+      return lose();
+    } else {
+      return win();
+    }
+  } else {
+    if (computerSelection === "rock") {
+      return lose();
+    } else {
+      return win();
     }
   }
-  
-  console.log(playRound(getComputerChoice(), getComputerChoice()));
+}
+
+function game() {
+  playerScore = 0;
+  computerScore = 0;
+  for (i = 1; i <= 5; i++) {
+    playerSelection = prompt("Type your selection (rock/paper/scissors):");
+    round = playRound(playerSelection, getComputerChoice());
+    console.log(round);
+    if (round.charAt(4) === "w") {
+      playerScore++;
+    } else if (round.charAt(4) === "l") {
+      computerScore++;
+    }
+    console.log(`The score is ${playerScore}-${computerScore}!`);
+  }
+  if (playerScore > computerScore) {
+    console.log("You won the game!");
+  } else if (playerScore < computerScore) {
+    console.log("You lost the game!");
+  } else {
+    console.log("The game is a draw!");
+  }
+}
+
+game();
